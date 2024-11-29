923013fbcccd3c393266521b98dc789cc54c3a66
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2024-11-29 16:40:59 +0100

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+OUTPUT=shadow_client
+
+rm -rf $OUTPUT
+
+# /home/kali/d3m0n_os_debian/rootfs/usr/share/d3m0n/src/settings.cpp is settings file
+g++ -o $OUTPUT main.cpp /home/kali/d3m0n_os_debian/rootfs/usr/share/d3m0n/src/settings.cpp  -std=c++17 -I. -lssl -lcrypto -lsqlite3 || exit 1
+
+chmod +x $OUTPUT
+clear
+
+./$OUTPUT $1 $2 $3 
