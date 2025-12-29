55b43dc8a0445882d61d4e54fd41cb8a70ab57ee
0x262d <71982379+canarddu38@users.noreply.github.com>
2025-12-29 20:22:54 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ OUTPUT=shadow_client
 
 rm -rf $OUTPUT
 
-g++ -o $OUTPUT main.cpp /home/kali/d3m0n_os_debian/rootfs/usr/share/d3m0n/src/settings.cpp -std=c++17 -I. -lssl -lcrypto  || exit 1
+g++ -o $OUTPUT main.cpp settings.cpp -std=c++17 -I. -lssl -lcrypto  || exit 1
 
 chmod +x $OUTPUT
 
